51d3cc5badc064e08ddf5bbe66b1b4c33cc34658
Hallion Benjamin <benbenmushi@gmail.com>
2019-10-27 12:41:00 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using ImageMagick;
+using System;
+using System.IO;
+
+namespace HEICtoJPG
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int fileLeft;
+			foreach (var arg in args)
+			{
+				Console.WriteLine("Checking directory: " + arg);
+				if (Directory.Exists(arg))
+				{
+					string exportPath = Path.Combine(arg, "jpg");
+					if (!Directory.Exists(exportPath))
+						Directory.CreateDirectory(exportPath);
+					var filesInFolder = Directory.GetFiles(arg);
+					fileLeft = filesInFolder.Length;
+					foreach (var file in filesInFolder)
+					{
+						if (Path.GetExtension(file).ToLower().Contains("heic"))
+							ConvertImage(file, exportPath);
+						Console.WriteLine(--fileLeft + " files left");
+					}
+				}
+			}
+			Console.WriteLine("Press AnyKey to continue");
+			Console.ReadKey();
+		}
+
+		static void ConvertImage(string heicImagePath, string exportPath)
+		{
+			string exportFilePath = Path.Combine(exportPath, Path.GetFileNameWithoutExtension(Path.GetFileName(heicImagePath))) + ".jpg";
+			Console.Write("Converting " + exportFilePath + "...");
+			using (MagickImage image = new MagickImage(heicImagePath))
+			{
+				image.Write(exportFilePath);
+				Console.WriteLine("Ok");
+			}
+		}
+	}
+}
